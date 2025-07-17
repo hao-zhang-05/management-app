b4cf90d13ba45394c207ba65033364c18d21d5e9
hao-zhang-05 <hao@mmtech.com.au>
2025-07-17 18:10:33 +1000

    basic skeleton

---
+++ components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+    return (
+        <aside>
+            <h2>Your projects</h2>
+            <div>
+                <button>New project</button>
+            </div>
+            <ul></ul>
+        </aside>
+    )
+}
