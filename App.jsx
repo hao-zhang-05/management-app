eedc249b71d838ea6fe31f283c01414e6f9bef0f
hao-zhang-05 <hao@mmtech.com.au>
2025-07-18 12:04:49 +1000

    'Save' button works, need to show the saved project in sidebar and close the 'NP' page

--- App.jsx
+++ App.jsx
@@ -1,11 +1,49 @@
+import { useState } from 'react';
+
 import Sidebar from './components/Sidebar';
+import NoProject from './components/NoProject';
 import NewProject from './components/NewProject';
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random().toString(),
+      };
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject}/>;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProject onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar />
-      <NewProject />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- components/NewProject.jsx
+++ components/NewProject.jsx
@@ -1,16 +1,42 @@
+import { useRef } from 'react';
 import Input from './Input';
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAdd({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate
+        });
+    }
+
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex item-center justify-end gap-4 my-4">
-                <li><button className="py-2 text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li>
+                    <button className="py-2 text-stone-800 hover:text-stone-950">
+                        Cancel
+                    </button>
+                </li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                        onClick={handleSave}>
+                        Save
+                    </button>
+                </li>
             </menu>
             <div>
-                <Input label="Title"/>
-                <Input label="Description" textArea />
-                <Input label="Due date" />
+                <Input type="text" ref={title} label="Title" />
+                <Input ref={description} label="Description" textArea />
+                <Input type="date" ref={dueDate} label="Due date" />
             </div>
         </div>
     )
